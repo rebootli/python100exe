0f6f655144bb023c3be5c492410677c562382c95
Tabe <2502715229@qq.com>
2025-03-12 14:56:58 +0800

    阶段性二

---
+++ 14.函数和模块.py
@@ -0,0 +1,29 @@
+'''
+m= int(input('m = '))
+n= int(input('n = '))
+
+fm = 1
+for num in range(1, m+1):
+    fm *= num
+
+fn = 1
+for num in range(1, n+1):
+    fn *= num
+
+fk=1
+for num in range(1, m-n+1):
+    fk *= num
+
+print(fm // fn // fk)
+'''
+
+def fac(num):
+    result = 1
+    for n in range(1, num + 1):
+        result *= n
+    return result
+
+m = int(input('m = '))
+n = int(input('n = '))
+
+print(fac(m) // fac(n) // fac(m - n))
